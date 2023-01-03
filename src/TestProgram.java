74f27f5c50a80a2c8da175f0ad9c0b2eacaecc4e
Dnyaneshwar <dnyaneshwar.shejul990@gmail.com>
2023-01-03 20:13:15 +0530

    Added code in TestProgram

--- src/TestProgram.java
+++ src/TestProgram.java
@@ -4,6 +4,8 @@ public class TestProgram {
 	public static void main(String[] args) {
 		
 		System.out.println("Test Pro");
+		System.out.println("Test Pro");
+		System.out.println("Test Pro");
 	}
 
 }
